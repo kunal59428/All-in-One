aaac7050f09a7723a945ed6f69adbf910c1348f3
kunal59428 <kumarsharmakunal@gmail.com>
2024-02-28 11:28:54 +0530

    changed

--- src/pages/donar/DonarReg/DonarRegister.jsx
+++ src/pages/donar/DonarReg/DonarRegister.jsx
@@ -40,7 +40,7 @@ const DonarRegister = () => {
         },
       };
       await axios
-        .post("https://all-in-one-rew7.onrender.com/api/consumer", value, config)
+        .post("https://all-in-one-rew7.onrender.com/api/register", value, config)
         .then((res) => console.log(res.data))
         .catch((err) => console.log(err));
     }
